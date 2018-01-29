584f7caac3e320a6c74940f1f1b8a651634b9de5
Pham Trung Nguyen <nguyen.pham@tripath.vn>
2018-01-29 17:17:45 +0700

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'about/about'
+
   get 'contact/contact'
 
-  resources :users
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
